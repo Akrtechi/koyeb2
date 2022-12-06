68025c623e6f521e69bcfd7c59b056b6130902cd
AKR <98525798+Akrtechi@users.noreply.github.com>
2022-12-06 18:03:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/Akrtechi/DQ-the-file-donor.git /DQ-The-File-Donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /Akrtechi/DQ-the-file-donor
 fi
 cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
